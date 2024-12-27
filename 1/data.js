dca0c0cc7546f7f21284c172c5dfa32173c2d3b9
McNooKiee <as055239328@gmail.com>
2024-12-27 11:04:04 +0800

    lk;l.

---
+++ 1/data.js
@@ -0,0 +1,262 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-antique-automobile-museum",
+      "name": "Antique Automobile Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.3802105705658505,
+        "pitch": 0.024545485450122584,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2942626030973248,
+          "pitch": 0.31287430941996774,
+          "rotation": 5.497787143782138,
+          "target": "1-edge-motor-museum"
+        },
+        {
+          "yaw": 2.614347213713563,
+          "pitch": 0.2622354926832262,
+          "rotation": 5.497787143782138,
+          "target": "4-marconi-automotive-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7165689348654443,
+          "pitch": 0.44241993399349333,
+          "title": "Antique Automobile Museum",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-edge-motor-museum",
+      "name": "Edge Motor Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "yaw": 1.8752067583194778,
+        "pitch": 0.24182175922544147,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.921684291175456,
+          "pitch": 0.27698358682714286,
+          "rotation": 0,
+          "target": "0-antique-automobile-museum"
+        },
+        {
+          "yaw": 2.4151538911747394,
+          "pitch": 0.25074055424073904,
+          "rotation": 12.566370614359176,
+          "target": "2-glion-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1570528700525493,
+          "pitch": -0.3232050761638483,
+          "title": "Edge Motor Museum",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-glion-museum",
+      "name": "Glion Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7397304174398283,
+        "pitch": -0.0513950838344428,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10490148907606667,
+          "pitch": 0.06847408648185471,
+          "rotation": 0,
+          "target": "3-lemay---americas-car-museum"
+        },
+        {
+          "yaw": -3.136537425834053,
+          "pitch": 0.06677930467765236,
+          "rotation": 0,
+          "target": "1-edge-motor-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7120584253859246,
+          "pitch": -0.4951382510109994,
+          "title": "Glion Museum",
+          "text": "no photo inside"
+        }
+      ]
+    },
+    {
+      "id": "3-lemay---americas-car-museum",
+      "name": "LeMay - America’s Car Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.03519232157881014,
+        "pitch": 0.05305087929393082,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0064328638073050115,
+          "pitch": 0.21061803936697387,
+          "rotation": 0,
+          "target": "2-glion-museum"
+        },
+        {
+          "yaw": 3.104604394443804,
+          "pitch": 0.10533692208647061,
+          "rotation": 11.780972450961727,
+          "target": "4-marconi-automotive-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09639361220770581,
+          "pitch": -1.4703779847336698,
+          "title": "LeMay - America’s Car Museum",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-marconi-automotive-museum",
+      "name": "Marconi Automotive Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6027976465594582,
+        "pitch": -0.0966819182985148,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5596986376442494,
+          "pitch": 0.0324010738621503,
+          "rotation": 0.7853981633974483,
+          "target": "0-antique-automobile-museum"
+        },
+        {
+          "yaw": -2.2668106860239874,
+          "pitch": 0.028019340501266754,
+          "rotation": 5.497787143782138,
+          "target": "3-lemay---americas-car-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8685896608675954,
+          "pitch": -0.6195330000759292,
+          "title": "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;right down",
+          "text": "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;\\/"
+        }
+      ]
+    }
+  ],
+  "name": "這個老師是傻逼",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ 2/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-newport-car-museum",
+      "name": "Newport Car Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.4491029237651425,
+        "pitch": 0.18049230193719623,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6262402091788282,
+          "pitch": 0.10557392530705911,
+          "rotation": 0.7853981633974483,
+          "target": "1-nissan-heritage-collection"
+        },
+        {
+          "yaw": 2.708175066343859,
+          "pitch": 0.05235158579588983,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7211592484547111,
+          "pitch": 0.3688489054607853,
+          "title": "Newport Car Museum",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-nissan-heritage-collection",
+      "name": "Nissan Heritage Collection",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1792,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.055909342179569776,
+          "pitch": 0.1632340604310336,
+          "rotation": 0,
+          "target": "0-newport-car-museum"
+        },
+        {
+          "yaw": -3.0868052666190113,
+          "pitch": 0.20638889261944726,
+          "rotation": 0,
+          "target": "2-seal-cove-auto-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.24768183147423173,
+          "pitch": 1.363538847136196,
+          "title": "Nissan Heritage Collection",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-seal-cove-auto-museum",
+      "name": "Seal Cove Auto Museum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6084415193766866,
+        "pitch": 0.02647796275860692,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6439910973846672,
+          "pitch": 0.0582876207886045,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.8232773351623557,
+          "pitch": 0.15389176159256301,
+          "rotation": 0,
+          "target": "1-nissan-heritage-collection"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6488434685330269,
+          "pitch": -0.06350235216051381,
+          "title": "Seal Cove Auto Museum",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "吉尔摩汽车博物馆",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.4616964100566605,
+        "pitch": 0.13721776034501687,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4146566853324867,
+          "pitch": 0.07493127979000747,
+          "rotation": 0,
+          "target": "2-seal-cove-auto-museum"
+        },
+        {
+          "yaw": 1.7433504697731905,
+          "pitch": 0.13923872677653648,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.104863358340957,
+          "pitch": -0.435888194657144,
+          "title": "吉尔摩汽车博物馆",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "彼得森汽車博物館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.1704540149219262,
+        "pitch": 0.31270251460959386,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5916968479717042,
+          "pitch": 0.14482941190750154,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.9029035539371701,
+          "pitch": 0.14351532212597462,
+          "rotation": 0.7853981633974483,
+          "target": "0-newport-car-museum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5408851598873037,
+          "pitch": -0.09920663694060394,
+          "title": "彼得森汽車博物館",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
